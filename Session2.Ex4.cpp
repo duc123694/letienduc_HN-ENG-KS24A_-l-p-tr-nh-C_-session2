99c13ce596899117319c8d53d82c53c776e516af
duc123694 <tienducle86@gmail.com>
2024-11-20 23:59:49 +0700

    bài tập session02-4

---
+++ Session2.Ex4.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+int canh = 5;
+printf("Chieu Canh Hinh Vuong: %d \n", canh);
+float chuvi = 4 * canh;
+printf("Chu Vi Hinh Vuong: 4 x %d = %.2f \n", canh, chuvi);
+float dientich = canh * canh;
+printf("Dien Tich Hinh Vuong: %d x %d = %.2f \n", canh, canh, dientich);
+return 0;
+}
